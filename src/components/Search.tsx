7bb0403482cc3e7e17e94d108d34c973c86ce616
VernalEquinox <pieroapruzzese@gmail.com>
2021-02-15 01:04:16 +0100

    added state and postcode in the search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -40,8 +40,8 @@ import { IWeather, Clouds, Main, Weather } from "../types/interfaces";
 
 const Search = (props: RouteComponentProps) => {
   const [cityResult, setCityResult] = useState<string>("Rome");
-  const [stateResult, setStateResult] = useState<string | null>("Italy");
-  const [setCode, setStateCode] = useState<string | null>("00100");
+  const [stateResult, setStateResult] = useState<string | "">("Italy");
+  const [codeResult, setCodeResult] = useState<string | "">("00100");
   const [weathers, setWeathers] = useState<IWeather | null>(null);
   const [clouds, setClouds] = useState<Clouds | null>(null);
   const [temperature, setTemperature] = useState<Main | null>(null);
@@ -64,7 +64,9 @@ const Search = (props: RouteComponentProps) => {
       let weather = data.weather;
       setWeather(weather);
 
-      setSearchResult("");
+      setCityResult("");
+      setStateResult("");
+      setCodeResult("");
 
       console.log(weathers);
     } catch (error) {
@@ -72,49 +74,58 @@ const Search = (props: RouteComponentProps) => {
     }
   };
 
-  puppa = (e) => {
-    e.preventDefault();
-    fetchForecast();
+  /*const cityHandler = (e: React.FormEvent<HTMLInputElement>) => {
+    setCityResult(e.currentTarget.value);
   };
+
+  const stateHandler = (e: React.FormEvent<HTMLInputElement>) => {
+    setStateResult(e.currentTarget.value);
+  };
+
+  const codeHandler = (e: React.FormEvent<HTMLInputElement>) => {
+    setCodeResult(e.currentTarget.value);
+  };
+
+  const Search = (e: React.FormEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    let puppa = fetchForecast;
+  }; */
 
   return (
     <div>
       <Container>
         <div className="d-flex justify-content-center">
           <InputGroup className="my-3 w-50">
-            <Form
-              inline
-              onSubmit={() => {
-                puppa();
-              }}
-            >
+            <Form inline>
               <FormControl
                 placeholder="Search for a city..."
                 aria-label="Search for a city..."
                 aria-describedby="basic-addon2"
-                value={searchResult}
-                onChange={(e) => setSearchResult(e.currentTarget.value)}
+                value={cityResult}
+                onChange={(e) => setCityResult(e.currentTarget.value)}
               />
               <FormControl
-                placeholder="Search for a city..."
-                aria-label="Search for a city..."
+                placeholder="Search for a state..."
+                aria-label="Search for a state..."
                 aria-describedby="basic-addon2"
-                value={searchResult}
-                onChange={(e) => setSearchResult(e.currentTarget.value)}
+                value={stateResult}
+                onChange={(e) => setStateResult(e.currentTarget.value)}
               />
               <FormControl
-                placeholder="Search for a city..."
-                aria-label="Search for a city..."
+                placeholder="Search for a postcode..."
+                aria-label="Search for a postcode..."
                 aria-describedby="basic-addon2"
-                value={searchResult}
-                onChange={(e) => setSearchResult(e.currentTarget.value)}
+                value={codeResult}
+                onChange={(e) => setCodeResult(e.currentTarget.value)}
               />
               <InputGroup.Append>
                 <Button
                   variant="outline-light"
-                  onClick={() => fetchForecast(searchResult)}
+                  onClick={(e) =>
+                    fetchForecast(cityResult, stateResult, codeResult)
+                  }
                 >
-                  City
+                  Search
                 </Button>
               </InputGroup.Append>
             </Form>
